7a9162170f3c113f58c1e1c99a3ae9bdb19afb0b
Nathan Relyea <relyean19@up.edu>
2019-05-08 00:31:38 -0700

    basic encoding working
    no size encoding, just the base message

--- PicEncrypt.py
+++ PicEncrypt.py
@@ -1,4 +1,6 @@
 import cv2
+import ctypes
+import sys
 
 
 def reformat_binary(s):
@@ -11,8 +13,12 @@ def reformat_binary(s):
         if len(binary_list[i]) < 7:
             binary_list[i] = zeros[0:(7 - len(binary_list[i]))] + binary_list[i]
 
-    s = " ".join(str(x) for x in binary_list)
-    return s
+    # s = " ".join(str(x) for x in binary_list)
+    return binary_list
+
+def bin2text(s): return "".join([chr(int(s[i:i+8],2)) for i in range(0,len(s),8)])
+
+def output(s): return ctypes.windll.user32.MessageBoxW(0, s, "Error", 1)
 
 
 img = cv2.imread('babbybeagle.jpg')
@@ -22,24 +28,45 @@ height, width = img.shape[:2]
 print("height: ", height)
 print("width: ", width)
 
-str_input = "Hello World! ayy."
+with open('input.txt', 'r') as file:
+    str_input = file.read().replace('\n', '')
+print("data:", str_input)
 
 str_binary = ' '.join(["{0:b}".format(x) for x in bytes(str_input, "ascii")])
-str_binary = reformat_binary(str_binary)
+
+binary_list = reformat_binary(str_binary)
+
+# verify encoding is correct
+if str_input == bin2text(" ".join(str(x) for x in binary_list)):
+    print("binary conversion successful")
+else:
+    output("Binary Encoding Error\n" + str_input + " -> " + bin2text(" ".join(str(x) for x in binary_list)))
+    sys.exit()
+
+print("reformatted:", " ".join(str(x) for x in binary_list))
 
 f = open("binary.txt", "w")
 f.write(str_binary)
 f.close()
 
-print(str_binary)
-
-n = 0
+byte = 0
+bit = 0
 
 for i in range(0, height):
     for j in range(0, width):
-        val = str_binary[n]
-        if img[i][j][0] % 2 == 0:
-            img[i][j][0] = 150
+        print("encoding ", binary_list[byte][bit])
+        if img[i][j][0] % 2 == 0 and int(binary_list[byte][bit]) == 1:
+            img[i][j][0] += 1
+        elif img[i][j][0] % 2 == 1 and int(binary_list[byte][bit]) == 0:
+            img[i][j][0] += 1
+        bit += 1
+        if bit == 7:
+            bit = 0
+            byte += 1
+        if byte > len(binary_list) - 1:
+            break
+    if byte > len(binary_list) - 1:
+        break
 
 
 cv2.imshow('image', img)
